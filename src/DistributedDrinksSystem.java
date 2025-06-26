eb0f8511c5a1ddec1c7c5e09e5a20e729c9c8726
mitchellkoech <mitchikoechi3.7@gmail.com>
2025-06-26 14:47:30 +0300

    Created core models of this system
    Signed-off-by: mitchellkoech <mitchikoechi3.7@gmail.com>

--- src/DistributedDrinksSystem.java
+++ src/DistributedDrinksSystem.java
@@ -1,1 +1,34 @@
-git
+import java.time.LocalDateTime;
+import java.util.*;
+import java.time.localDateTime;
+
+class Drink {
+    String id;
+    String name;
+    double price;
+    int quantity;
+    public Drink(String id, String name, double price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
+
+class Order {
+    String orderId;
+    String customerName;
+    String branch;
+    List<Drink> drinksOrdered;
+    double totalPrice;
+    LocalDateTime orderTime;
+
+    public Order(String orderId, String customerName, String branch, List<Drink> drinksOrdered, double totalPrice){
+        this.orderId = orderId;
+        this.customerName = customerName;
+        this.branch = branch;
+        this.drinksOrdered = drinksOrdered;
+        this.totalPrice = totalPrice;
+        this.orderTime = LocalDateTime;
+    }
+}
