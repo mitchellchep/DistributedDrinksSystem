c930dfd0c227bccd1658981a0c758753bf0c49fe
mitchellkoech <mitchikoechi3.7@gmail.com>
2025-06-26 15:05:59 +0300

    BRanch management
    Signed-off-by: mitchellkoech <mitchikoechi3.7@gmail.com>

--- src/DistributedDrinksSystem.java
+++ src/DistributedDrinksSystem.java
@@ -31,4 +31,53 @@ class Order {
         this.totalPrice = totalPrice;
         this.orderTime = LocalDateTime;
     }
+
+    //Branch manangement
+    class Branch {
+        String name;
+        Map<String, Drink> stock = new HashMap<>();
+        List<Order> orders = new ArrayList<>();
+        final int MIN_THRESHOLD = 10;
+
+        public Branch(String name) {
+            this.name = name;
+        }
+
+        public void addDrink(Drink drink) {
+            stock.put(drink.id, drink);
+        }
+
+        public boolean processOrder(Order order){
+            for (Drink d: order.drinksOrdered) {
+                Drink branchDrink = stock.get(d.id);
+                if (branchDrink == null || branchDrink.quantity <d.quantity>)
+                    return false;
+            }
+            //Deduct stock and store order
+            for (Drink d : order.drinksOrdered){
+                stock.get(d.id).quantity -= d.quantity;
+            }
+            orders.add(order);
+            checkStockLevels();
+            return true;
+        }
+
+        public void checkStockLevels() {
+            for (Drink d: stock.values()){
+                if(d.quantity < MIN_THRESHOLD){
+                    System.out.println("[Low stock alert] Drink:"+d.name+"at"+name);
+                }
+            }
+        }
+        public double getTotalSales(){
+            return orders.stream().mapToDouble(o->o.totalPrice).sum();
+        }
+        public void printBranchReport(){
+            System.out.println("\n--- Sales report for branch:"+name+"---");
+            for (Order o :orders){
+                System.out.println("OrderID:"+o.orderId+", Customer: "+o.customerName+", Total: KES "+o.totalPrice);
+            }
+            System.out.println("Total sales: KES "+getTotalSales());
+        }
+    }
 }
